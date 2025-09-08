22d69818ca6e7a95e14a08f4d08621d95216f1a7
unknown <pje224@naver.com>
2025-09-08 14:37:03 +0900

    서명하기 영역 완료, fixed 버튼 js 작성해야됨

--- js/pje.js
+++ js/pje.js
@@ -1,1 +1,14 @@
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+  const signSwiper = new Swiper(".sign_swiper", {
+    direction: "vertical",
+    spaceBetween: 30,
+    // centeredSlides: true,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    // // speed: 5000,
+  });
+});
