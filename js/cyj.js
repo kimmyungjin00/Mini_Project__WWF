507ad5aef0e5857a8020b0bcb392a41e0a963e06
unknown <pje224@naver.com>
2025-09-12 16:26:24 +0900

    전반적인 수정사항(반응형 , 스크롤 문제 등) 작업

--- js/cyj.js
+++ js/cyj.js
@@ -2,38 +2,52 @@ window.addEventListener("load", () => {
   // Register GSAP Plugins
   gsap.registerPlugin(ScrollTrigger);
   // Parallax Layers
-  document.querySelectorAll("[data-parallax-layers]").forEach((triggerElement) => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerElement,
-        start: "0% 0%",
-        end: "100% 0%",
-        scrub: 0,
-      },
-    });
-    const layers = [
-      { layer: "1", yPercent: 70 },
-      { layer: "2", yPercent: -80 },
-    ];
-    layers.forEach((layerObj, idx) => {
-      tl.to(
-        triggerElement.querySelectorAll(`[data-parallax-layer="${layerObj.layer}"]`),
-        {
-          yPercent: layerObj.yPercent,
-          ease: "none",
+  document
+    .querySelectorAll("[data-parallax-layers]")
+    .forEach((triggerElement) => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: triggerElement,
+          start: "0% 0%",
+          end: "100% 0%",
+          scrub: 0,
         },
-        idx === 0 ? undefined : "<"
-      );
+      });
+      const layers = [
+        { layer: "1", yPercent: 70 },
+        { layer: "2", yPercent: -80 },
+      ];
+      layers.forEach((layerObj, idx) => {
+        tl.to(
+          triggerElement.querySelectorAll(
+            `[data-parallax-layer="${layerObj.layer}"]`
+          ),
+          {
+            yPercent: layerObj.yPercent,
+            ease: "none",
+          },
+          idx === 0 ? undefined : "<"
+        );
+      });
     });
-  });
   /* Lenis */
-  const lenis = new Lenis();
-  lenis.on("scroll", ScrollTrigger.update);
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 1000);
-  });
-  gsap.ticker.lagSmoothing(0);
+  gsap.registerPlugin(ScrollTrigger);
 
+  ScrollTrigger.scrollerProxy(document.body, {
+    scrollTop(value) {
+      return arguments.length
+        ? lenis.scrollTo(value)
+        : lenis.scroll.instance.scroll;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+  });
   // 헤더메뉴 스크롤했을때 나오게하기
   const header = document.querySelector(".header");
   const hamburger = document.querySelector(".hamburger");
